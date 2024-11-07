cc683d7eeb8e9e97c2f8f4554fe69a9c728f6f16
Nitesh Chauhan <nitesh.c@caratlane.com>
2024-11-07 18:04:59 +0530

    added start game function where game is going to start from

--- numberGuessingGame.js
+++ numberGuessingGame.js
@@ -11,3 +11,27 @@ const difficultyLevels = {
     hard: 3
 }
 
+function startGame() {
+    read.question("Enter difficulty level (easy, medium, hard): ", (level) => {
+        // In Node.JS below chances variable will be 'undefined' .
+        //In Node.js falsy values are-
+        //false
+        // 0 (the number zero)
+        // "" (an empty string)
+        // null
+        // undefined
+        // NaN (Not a Number)
+        const chances = difficultyLevels[level.toLowerCase()];
+
+        if (!chances) {
+            console.log("Invalid Input.")
+            //startGuessing()
+            startGame()
+        } else {
+            console.log("We will start with - " + chances)
+            startGuessing(chances)
+        }
+
+    })
+}
+
