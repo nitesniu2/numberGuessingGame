8f6eb3cc5e691b61e3ea186e66929f03b11c7ace
Nitesh Chauhan <nitesh.c@caratlane.com>
2024-11-07 17:34:00 +0530

    added realine for commandline input

---
+++ numberGuessingGame.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+
+const read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const difficultyLevels = {
+    easy: 10,
+    medium: 7,
+    hard: 3
+}
+
